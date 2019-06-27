f7750637abed189f16aa511487c8c36824c842ef
Matteo Muscas <thatman.j@gmail.com>
2019-06-27 11:21:40 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,23 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113011820) do
+ActiveRecord::Schema.define(version: 2016_01_13_011820) do
 
   create_table "authors", force: :cascade do |t|
-    t.string   "name"
-    t.string   "genre"
-    t.text     "bio"
+    t.string "name"
+    t.string "genre"
+    t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.boolean  "post_status"
-    t.integer  "author_id"
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "post_status"
+    t.integer "author_id"
   end
 
 end
